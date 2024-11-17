5eb89e65c5a8f7e53ba8373009559f3ea1e02e19
Shahmeer0311 <22alis@students.watfordboys.org>
2024-11-17 14:45:04 +0000

    String opertion

---
+++ main.py
@@ -0,0 +1,12 @@
+text = input("Enter a string-")
+print("reverse of the given string",text[::-1])
+print("first letter of the given string ",text[0])
+print("last letter of the given string ",text[-1])
+print("Length of the given string ",len(text))
+x = "JohnAbadon"
+print(x[0:-1:2])
+print(x[0::1])
+print(x[::1])
+y = "AbadonJohn"
+print(x + y)
+print(y.upper())
